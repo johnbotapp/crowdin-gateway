950b89c8ab2bbcf4c15f69a31394a208c32e0457
Alexis <87914117+Arhusien@users.noreply.github.com>
2024-04-13 23:18:58 -0400

    Initial commit

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span>An error occurred: {{ error }}</span>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    error: {
+      type: Object,
+      required: true
+    }
+  });
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div class="container flex min-h-screen items-center justify-center">
+      <h1 class="text-center font-serif text-3xl">Voix ambiguë d'un coeur qui, au zéphyr, préfère les jattes de kiwis.</h1>
+    </div>
+  </div>
+</template>
